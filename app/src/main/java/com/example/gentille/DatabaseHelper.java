6792e2866cf5414fb379ea53376c4d0c93371ad4
thimote <thimoteeharerimana@gmail.com>
2019-05-17 22:59:28 +0200

    Initial commit

---
+++ app/src/main/java/com/example/gentille/DatabaseHelper.java
@@ -0,0 +1,28 @@
+package com.example.gentille;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "gentille.db";
+    public static final String TABLE_NAME = "gentille";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "username";
+    public static final String COL_3 = "password";
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(" CREATE  TABLE " + TABLE_NAME + "(ID INTEGER   PRIMARY KEY  AUTOINCREMENT  ,username TEXT, password TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(" DROP   TABLE  IF  EXISTS  " +TABLE_NAME);
+        onCreate(db);
+    }
+}
